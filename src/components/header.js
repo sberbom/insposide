9a849aadad4795f2d414f68c12ed0dc0e63d0f89
Sigmund Berbom <sberbom@gmail.com>
2020-04-03 11:47:58 +0200

    udated header video

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header(props) {
         return(
             <div>
                 <div className="header-container" style={{backgroundImage: `url(${props.backgroundImage})`}}>
-                    <iframe className="header-video" allowFullScreen="allowfullscreen" src={"https://www.youtube.com/embed/UxSOJVjO28o?autoplay=1"} title="headervideo"></iframe>
+                    <iframe className="header-video" allowFullScreen="allowfullscreen" src={"https://www.youtube.com/embed/aUFxiIip8XY"} title="headervideo"></iframe>
                 </div>
                 <h1 className="display-4 main-title">{props.text}</h1>
             </div>
